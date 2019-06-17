fed9ede197eb894b0ab09209c3f7af1ba63b8b82
rafanogueira <50391609+rafanogueira@users.noreply.github.com>
2019-06-17 19:17:11 -0400

    Update composer-run.sh

--- composer-run.sh
+++ composer-run.sh
@@ -3,10 +3,7 @@
 # Accessing the project folder.
 cd /var/lib/jenkins/workspace/Tipickly\ API\ Deploy\ \(PROD\)/
 
-# Running composer install.
-sudo php -dmemory_limit=1024M composer install
-
 # Running composer update.
-sudo php -dmemory_limit=1024M composer update
+composer update
 
 # Rsyncing to the server
