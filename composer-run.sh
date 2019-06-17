284e3c04d87863dd7fe35f7ba1ffe87be3bf79e5
rafanogueira <50391609+rafanogueira@users.noreply.github.com>
2019-06-17 19:44:03 -0400

    Update composer-run.sh

--- composer-run.sh
+++ composer-run.sh
@@ -6,4 +6,9 @@ cd /var/lib/jenkins/workspace/Tipickly\ API\ Deploy\ \(PROD\)/
 # Running composer update.
 composer install && composer update
 
+# Zipping the folder.
+cd ..
+zip -r tpanel.zip Tipickly\ API\ Deploy\ \(PROD\)/
+
 # Rsyncing to the server
+rsync -avz tpanel.zip root@68.183.192.36:/var/www
