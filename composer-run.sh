ad174aacf6eb2770e1903ab0e2e5b716233a9c5f
rafanogueira <50391609+rafanogueira@users.noreply.github.com>
2019-06-17 18:36:42 -0400

    Update composer-run.sh
    Adding Rsync comment

--- composer-run.sh
+++ composer-run.sh
@@ -5,3 +5,10 @@ cd /var/lib/jenkins/workspace/Tipickly\ API\ Deploy\ \(PROD\)/
 
 # Running composer install.
 composer install
+
+# Rsyncing to the server
+
+# Deleting the folders after completion
+cd ..
+sudo rm -Rf Tipickly\ API\ Deploy\ \(PROD\)/
+sudo rm /var/www/tipickly-ci
