4f5a202d7c4b238ccb5af7729baba880bca57069
rafanogueira <50391609+rafanogueira@users.noreply.github.com>
2019-06-17 19:07:57 -0400

    Update composer-run.sh

--- composer-run.sh
+++ composer-run.sh
@@ -9,6 +9,6 @@ composer install
 # Rsyncing to the server
 
 # Deleting the folders after completion
-cd ..
-sudo rm -Rf /var/lib/jenkins/workspace/Tipickly\ API\ Deploy\ \(PROD\)/
-sudo rm -Rf /var/www/tipickly-ci
+# cd ..
+# sudo rm -Rf /var/lib/jenkins/workspace/Tipickly\ API\ Deploy\ \(PROD\)/
+# sudo rm -Rf /var/www/tipickly-ci
